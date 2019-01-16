b5506e54ec8bff7c326644e34e275a2bd9e55d8a
Japneet Singh <jpntsingh8@gmail.com>
2019-01-16 14:09:03 +0530

    final commit

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
 
 console.log(screen.width, screen.height)
 console.log(window.innerWidth, window.innerHeight)
-let image = document.querySelector('img')
+
+let image = document.querySelector('.naruto')
 
 image.width = screen.width/(1.8)
 image.height = screen.height/(1.8)
+
+let post = document.querySelector('.directions')
+
+post.width = screen.width/(2.5)
 
 setTimeout(()=> {
     document.querySelector('body').style.overflowY = "auto"
